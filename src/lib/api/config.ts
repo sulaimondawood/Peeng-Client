411716bdb33df1b3b0a2a81a7179fb24204b8d2a
sulaimondawood <sulaimondawod@gmail.com>
2026-07-28 20:36:03 +0100

    modified githun url

--- src/lib/api/config.ts
+++ src/lib/api/config.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError } from "axios";
 import { clearAuth, getAccessToken, getLastTenantId } from "./auth-storage";
 
-const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api/v1';
+const baseURL = import.meta.env.VITE_API_BASE_URL || 'https://peeng-client.vercel.app/api/v1';
 
 export const api = axios.create({
     baseURL,
